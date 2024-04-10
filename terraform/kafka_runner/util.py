95aa1a7e9f95e8083f1470804c662d853f2fb19e
Rashi-Confluent <raagrawal@confluent.io>
2024-04-10 15:24:55 +0530

    update_region_and_ami

--- terraform/kafka_runner/util.py
+++ terraform/kafka_runner/util.py
@@ -16,15 +16,15 @@ ABS_KAFKA_DIR = os.path.abspath(BASE_KAFKA_DIR)
 
 logging.getLogger("paramiko").setLevel(logging.WARNING)
 
-AWS_REGION = "us-west-1"
+AWS_REGION = "us-west-2"
 AWS_ACCOUNT_ID = boto3.client('sts', region_name=AWS_REGION).get_caller_identity().get('Account')
 AWS_IAM = boto3.client('sts', region_name=AWS_REGION).get_caller_identity().get('Arn').split("/")[1]
 AMI_NAME_MAX_LENGTH = 128
 WORKER_AMI_NAME = 'kafka-{}'.format(  # E.g. BUILD_TAG = jenkins-system-test-confluent-platform-master-452
     os.environ['BUILD_TAG'][8:] if 'BUILD_TAG' in os.environ else str(int(time.time())))
 
-AMI= "ami-29ebb519"
-INSTANCE_TYPE= "c4.xlarge"
+AMI= "ami-5189a661"
+INSTANCE_TYPE= "m3.xlarge"
 
 # Various ways to install the confluent platform components
 SOURCE_INSTALL = "source"
