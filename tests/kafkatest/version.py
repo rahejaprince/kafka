dc07cb4c82b63585be4779f103090401168d1aeb
ConfluentSemaphore <40306929+ConfluentSemaphore@users.noreply.github.com>
2024-07-02 16:38:48 +0000

    chore: Set project version and resolve dependencies. 7.6.2-25-ccs

--- tests/kafkatest/version.py
+++ tests/kafkatest/version.py
@@ -122,7 +122,7 @@ def get_version(node=None):
         return DEV_BRANCH
 
 DEV_BRANCH = KafkaVersion("dev")
-DEV_VERSION = KafkaVersion("7.6.2-0-ccs")
+DEV_VERSION = KafkaVersion("7.6.2-25")
 
 LATEST_METADATA_VERSION = "3.6"
 
